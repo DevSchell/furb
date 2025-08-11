91c266aa206b6bede975fb58622dcf7191b10cf4
SchellDev <schelldev@gmail.com>
2025-08-11 09:30:21 -0300

    fix: mudanças em alguns arquivos. Adição de validações nos exercícios

--- programacao_orientada_a_objetos/ListaExercicios1/Questao2/App.java
+++ programacao_orientada_a_objetos/ListaExercicios1/Questao2/App.java
@@ -1,20 +1,31 @@
 package programacao_orientada_a_objetos.ListaExercicios1.Questao2;
+
 import java.util.Locale;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        input.useLocale(Locale.US); 
+        input.useLocale(Locale.US);
 
         Pessoa user = new Pessoa();
 
-        System.out.println("Informe o seu peso em kg: ");
-        user.peso = input.nextDouble();
-        
-        System.out.println("Informe a sua altura em metros: ");
-        user.altura = input.nextDouble();
-        
+        while (user.peso <= 0) {
+            System.out.println("Informe o seu peso em kg: ");
+            user.peso = input.nextDouble();
+            if (user.peso <= 0) {
+                System.out.println("O peso informado deve ser maior do que zero");
+            }
+        }
+
+        while (user.altura <= 0) {
+            System.out.println("Informe a sua altura em metros: ");
+            user.altura = input.nextDouble();
+            if (user.altura <= 0) {
+                System.out.println("A altura informada deve ser maior do que zero");
+            }
+        }
+
         double imc = user.calcularImc();
 
         System.out.println("Seu IMC é: " + imc);
--- programacao_orientada_a_objetos/ListaExercicios1/Questao3/App.java
+++ programacao_orientada_a_objetos/ListaExercicios1/Questao3/App.java
@@ -13,35 +13,65 @@ public class App {
         Pessoa pessoa3 = new Pessoa();
 
         // Pessoa 1
-        System.out.println("Informe a altura da pessoa 1: ");
-        pessoa1.altura = input.nextDouble();
+        while (pessoa1.altura <= 0) {
+            System.out.println("Informe a altura da pessoa 1: ");
+            pessoa1.altura = input.nextDouble();
+            if (pessoa1.altura <= 0) {
+                System.out.println("A altura informada deve ser maior do que 0");
+            }
+        }
 
-        System.out.println("Informe o peso da pessoa 1: ");
-        pessoa1.peso = input.nextDouble();
+        while (pessoa1.peso <= 0) {
+            System.out.println("Informe o peso da pessoa 1: ");
+            pessoa1.peso = input.nextDouble();
+            if (pessoa1.peso <= 0) {
+                System.out.println("O peso informado deve ser maior do que 0");
+            }
+        }
 
         double imcPessoa1 = pessoa1.calcularImc();
         System.out.println("Seu IMC é: " + imcPessoa1);
         System.out.println();
 
         // Pessoa 2
-        System.out.println("Informe a altura da pessoa 2: ");
-        pessoa2.altura = input.nextDouble();
+        while (pessoa2.altura <= 0) {
+            System.out.println("Informe a altura da pessoa 2: ");
+            pessoa2.altura = input.nextDouble();
+            if (pessoa1.altura <= 0) {
+                System.out.println("A altura informada deve ser maior do que 0");
+            }
+        }
 
-        System.out.println("Informe o peso da pessoa 2: ");
-        pessoa2.peso = input.nextDouble();
+        while (pessoa2.peso <= 0) {
+            System.out.println("Informe o peso da pessoa 2: ");
+            pessoa2.peso = input.nextDouble();
+            if (pessoa2.peso <= 0) {
+                System.out.println("O peso informado deve ser maior do que 0");
+            }
+        }
 
         double imcPessoa2 = pessoa2.calcularImc();
         System.out.println("Seu IMC é: " + imcPessoa2);
         System.out.println();
 
         // Pessoa 3
-        System.out.println("Informe a altura da pessoa 3: ");
-        pessoa3.altura = input.nextDouble();
+        while (pessoa3.altura <= 0) {
+            System.out.println("Informe a altura da pessoa 3: ");
+            pessoa3.altura = input.nextDouble();
+            if (pessoa3.altura <= 0) {
+                System.out.println("A altura informada deve ser maior do que 0");
+            }
+        }
 
-        System.out.println("Informe o peso da pessoa 3: ");
-        pessoa3.peso = input.nextDouble();
+        while (pessoa3.peso <= 0) {
+            System.out.println("Informe o peso da pessoa 3: ");
+            pessoa3.peso = input.nextDouble();
+            if (pessoa3.peso <= 0) {
+                System.out.println("O peso informado deve ser maior do que 0");
+            }
+        }
 
-        double imcPessoa3 = pessoa2.calcularImc();
+        double imcPessoa3 = pessoa3.calcularImc();
         System.out.println("Seu IMC é: " + imcPessoa3);
         System.out.println();
         input.close();
