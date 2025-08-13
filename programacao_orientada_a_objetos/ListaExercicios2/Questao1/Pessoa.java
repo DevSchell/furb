a90310c95ff192d4764e2afd2cc862d25ca8baa8
SchellDev <schelldev@gmail.com>
2025-08-12 21:37:13 -0300

    nova lista de exercícios iniciada

---
+++ programacao_orientada_a_objetos/ListaExercicios2/Questao1/Pessoa.java
@@ -0,0 +1,39 @@
+package programacao_orientada_a_objetos.ListaExercicios2.Questao1;
+
+public class Pessoa {
+    private String nome;
+    private double peso;
+    private double altura;
+
+    //Ações
+    double calcularImc() {
+        return peso / (altura * altura);
+    }
+
+    //Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    //Getters
+    public String getNome() {
+        return this.nome;
+    }
+
+    public double getPeso() {
+        return this.peso;
+    }
+
+    public double getAltura() {
+        return this.altura;
+    }
+
+}
